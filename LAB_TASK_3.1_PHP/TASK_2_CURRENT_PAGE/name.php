b2a8fc6f4dcff044ef48915442e0a45da36dfa31
unknown <anjummishu8@gmail.com>
2020-11-03 00:57:07 +0600

    php

---
+++ LAB_TASK_3.1_PHP/TASK_2_CURRENT_PAGE/name.php
@@ -0,0 +1,40 @@
+
+<?php
+
+if(isset($_REQUEST['submit'])){
+
+    $name = $_REQUEST['name'];
+    
+    if($name ==""){
+        
+        echo "field required....";
+        
+    }
+    
+    else{
+        echo $name;
+    }
+    
+    
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Name</title>
+</head>
+<body>
+
+    <form method="post" action="name.php" style=" width:300px">
+        <fieldset>
+            <legend>Name</legend>
+            <input type="text" name="name" value="">
+            <hr>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+</html>
