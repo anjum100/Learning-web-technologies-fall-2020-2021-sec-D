344f57374e2a145e1ede84edce7d917ff09640de
unknown <anjummishu8@gmail.com>
2020-11-04 12:07:30 +0600

    php

---
+++ LAB_TASK_3.1_PHP/TASK_2_CURRENT_PAGE/bloodGroup.php
@@ -0,0 +1,43 @@
+<?php
+
+	if(isset($_REQUEST['submit']))
+	{
+		echo $_REQUEST['bloodGroup'];
+
+	}
+	
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BLOOD GROUP</title>
+</head>
+<body>
+
+	<form method="post" style=" width:300px">
+		<fieldset>
+			<legend> <h3><b>BLOOD GROUP </b></h3> </legend>
+			
+
+	   	
+	            <select name="Blood Group" >
+	            <option value="">A+</option>
+	            <option value="" selected ></option>
+				<option value="">A-</option>
+				<option value="">B+</option>
+				<option value="">B-</option>
+				<option value="">AB+</option>
+				<option value="">AB-</option>
+				<option value="">O+</option>
+				<option value="">O-</option>
+			</select> <br>
+			<hr>
+			<input type="submit" name="" value="Submit"> 
+             </fieldset><br>
+		</fieldset>
+	</form>
+</body>
+</html>
