e69b511960150c9b0100bdf65e8e70bbf5ddf59d
unknown <anjummishu8@gmail.com>
2020-11-04 13:03:38 +0600

    php

---
+++ LAB_TASK_3.1_PHP/TASK_3_INPUT_FIELD/email.php
@@ -0,0 +1,46 @@
+<?php
+
+
+$data="";
+
+
+if(isset($_REQUEST['submit'])){
+
+    $email = $_REQUEST['email'];
+    
+    if($email ==""){
+        
+        echo "field required....";
+        
+    }
+    
+    else{
+         $data=$email;
+    }
+    
+    
+}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email</title>
+</head>
+<body>
+	
+	<form method="post" style=" width:300px">
+		<fieldset>
+			<legend>EMAIL</legend>
+			<input type="text" name="email" value="">
+			<button title="hint:sample@example.com" style= "color:black;"> <b>i</b></button> <br><br>
+			<hr>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
