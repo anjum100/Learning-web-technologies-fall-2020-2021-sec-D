c72d0c51b03bc4a351f6faaecf863d3902b2b1f3
unknown <anjummishu8@gmail.com>
2020-11-11 00:38:27 +0600

    php

---
+++ LAB_TASk_4.1_SESSION/PublicHome.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PUBLIC HOME </title>
+</head>
+<body>
+	<form>
+		<table border ="1" width=70% align="center">
+			
+<tr>
+	<th colspan="3"><img width="70" align="left"   height="50" src="image/x.jpg">
+	 <a href="home.php" >Home| </a>
+	 <a href="login.php" >Login|</a>
+	 <a href="resistration.php" >Registration</a>
+
+</th>
+</tr>
+
+<tr height="250px">
+	<td width="70"> 
+	<h4>Welcome to xCompany</h4>
+</td>
+
+			<tr>
+				<td></td>
+			</tr>
+			<tr>
+				<td align ="center" colspan="4">Copyright @ 2017</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
+
+	
